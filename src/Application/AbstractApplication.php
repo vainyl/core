1de28b41fd7bc5c2191d03da5c885817b826029a
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-07 16:20:15 +0300

    Added AbstractApplication class

---
+++ src/Application/AbstractApplication.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Core\Application;
+
+/**
+ * Class AbstractApplication
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractApplication implements ApplicationInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function bootstrap(BootstrapperInterface $bootstrapper): ApplicationInterface
+    {
+        $bootstrapper->process($this);
+
+        return $this;
+    }
+}
