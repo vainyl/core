bfea8788e88e466553dcc24c1f36b2a24b04643a
Andrey Dembitskiy <andrey.dembitskiy@cosmonova.net>
2017-06-14 10:50:05 +0300

    :sparkles: php-ds set adapter (storage)

---
+++ src/Storage/Adapter/StorageSetAdapter.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Vainyl\Core\Storage\Adapter;
+
+use Ds\Set;
+use Vainyl\Core\AbstractIdentifiable;
+use Vainyl\Core\Storage\StorageInterface;
+
+/**
+ * Class StorageSetAdapter
+ *
+ * @author  Andrey Dembitskiy <andrey.dembitskiy@cosmonova.net>
+ * Cosmonova LLC
+ *
+ * @package Vainyl\Core\Storage\Adapter
+ */
+class StorageSetAdapter extends AbstractIdentifiable implements StorageInterface
+{
+    private $set;
+
+    /**
+     * StorageSetAdapter constructor.
+     *
+     * @param Set $set
+     */
+    public function __construct(Set $set)
+    {
+        $this->set = $set;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return $this->set->toArray();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        return new \IteratorIterator($this->set);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetExists($offset)
+    {
+        throw new \Error('Set data structure does not support checking for element');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetGet($offset)
+    {
+        return $this->set->offsetGet($offset);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->set->offsetSet($offset, $value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetUnset($offset)
+    {
+        throw new \Error('Set data structure does not support unset elements');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count()
+    {
+        return $this->set->count();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fromArray(array $configData): StorageInterface
+    {
+        $this->set->add(...$configData);
+
+        return $this;
+    }
+}
